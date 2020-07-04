1be641742ed52c175581a6c7be81174234619d83
Andriets <55383233+Andriets@users.noreply.github.com>
2020-07-04 15:03:52 +0200

    Day two

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ const me = {
   image:
     "https://images.unsplash.com/photo-1555445091-5a8b655e8a4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
   name: "Mykola",
-  age: 19
+  age: 19,
+  description: "Such a good boy <3"
 };
 
 function Image(props) {
-  console.log("Image props:", props);
   return <img width="300px" src={props.src} alt="" />;
 }
 
@@ -27,16 +27,50 @@ function Image(props) {
 // }
 
 class MyItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false,
+      like: false
+    };
+  }
+
+  button = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  like = () => {
+    this.setState({
+      like: !this.state.like
+    });
+  };
+
   render() {
     const {
-      data: { name, age, image }
+      data: { name, age, image, description }
     } = this.props;
-    console.log("MyItem context:", this);
     return (
-      <div>
+      <div className={"container"}>
         <h1>{name}</h1>
         <h2>{age} y.o.</h2>
         <Image src={image} />
+        <br />
+        <div className={"btns"}>
+          <button type="button" onClick={this.button}>
+            {this.state.show ? "hide" : "Description"}
+          </button>
+          <button
+            type="button"
+            onClick={this.like}
+            className={this.state.like ? "btn-like" : ""}
+          >
+            Like
+          </button>
+        </div>
+        {this.state.show ? <p>{description}</p> : null}
       </div>
     );
   }
